16ad2a80d652adbbbe2516c86b2648ac1f955184
Sebastian Nagel <sebastian.nagel@ncoding.at>
2016-10-16 23:49:40 +0200

    Added jwt_issue_algorithm directive

--- ngx_http_jwt_module.c
+++ ngx_http_jwt_module.c
@@ -2,13 +2,13 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_http.h>
-#include <jwt.h>
+#include <jwt/jwt.h>
 
 // Module structures
 
 typedef struct {
   ngx_flag_t issue;
-  jwt_alg_t algorithm;
+  ngx_uint_t algorithm;
 } ngx_http_jwt_loc_conf_t;
 
 typedef struct {
@@ -21,22 +21,38 @@ static char *ngx_http_jwt_merge_loc_conf(ngx_conf_t *cf,
                                          void *parent,
                                          void *child);
 static ngx_int_t ngx_http_jwt_init(ngx_conf_t *cf);
-// jwt_issue functions
-static char *ngx_http_jwt_issue(ngx_conf_t *cf,
-                                ngx_command_t *cmd,
-                                void *conf);
 static ngx_int_t ngx_http_jwt_issue_header_filter(ngx_http_request_t *request);
 static ngx_int_t ngx_http_jwt_issue_body_filter(ngx_http_request_t *request,
                                                 ngx_chain_t *chain);
+
+static ngx_conf_enum_t ngx_http_jwt_algorithms[] = {
+  { ngx_string("none"), JWT_ALG_NONE },
+  { ngx_string("HS256"), JWT_ALG_HS256 },
+  { ngx_string("HS384"), JWT_ALG_HS384 },
+  { ngx_string("HS512"), JWT_ALG_HS512 },
+  { ngx_string("RS256"), JWT_ALG_RS256 },
+  { ngx_string("RS384"), JWT_ALG_RS384 },
+  { ngx_string("RS512"), JWT_ALG_RS512 },
+  { ngx_string("ES256"), JWT_ALG_ES256 },
+  { ngx_string("ES384"), JWT_ALG_ES384 },
+  { ngx_string("ES512"), JWT_ALG_ES512 },
+  { ngx_null_string, 0 }
+};
 
 // Directives
 static ngx_command_t ngx_http_jwt_commands[] = {
   { ngx_string("jwt_issue"),
     NGX_HTTP_LOC_CONF|NGX_CONF_FLAG,
-    ngx_http_jwt_issue,
+    ngx_conf_set_flag_slot,
     NGX_HTTP_LOC_CONF_OFFSET,
     offsetof(ngx_http_jwt_loc_conf_t, issue),
     NULL },
+  { ngx_string("jwt_issue_algorithm"),
+    NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
+    ngx_conf_set_enum_slot,
+    NGX_HTTP_LOC_CONF_OFFSET,
+    offsetof(ngx_http_jwt_loc_conf_t, algorithm),
+    &ngx_http_jwt_algorithms },
   ngx_null_command
 };
 
@@ -85,6 +101,7 @@ static void * ngx_http_jwt_create_loc_conf(ngx_conf_t *cf) {
     return NULL;
   }
   conf->issue = NGX_CONF_UNSET;
+  conf->algorithm = NGX_CONF_UNSET_UINT;
 
   return conf;
 }
@@ -95,13 +112,21 @@ static char * ngx_http_jwt_merge_loc_conf(ngx_conf_t *cf, void *parent, void *ch
   ngx_http_jwt_loc_conf_t *conf = child;
 
   ngx_conf_merge_value(conf->issue, prev->issue, false);
+  ngx_conf_merge_uint_value(conf->algorithm, prev->algorithm, JWT_ALG_NONE);
 
   return NGX_CONF_OK;
 }
 
-// jwt_issue directive
-static char * ngx_http_jwt_issue(ngx_conf_t *cf, ngx_command_t *cmd, void *hint) {
-  return ngx_conf_set_flag_slot(cf, cmd, hint);
+// Post configuration - add request handler
+static ngx_int_t ngx_http_jwt_init(ngx_conf_t *cf) {
+  // Install jwt_issue_filter
+  ngx_http_next_header_filter = ngx_http_top_header_filter;
+  ngx_http_top_header_filter = ngx_http_jwt_issue_header_filter;
+
+  ngx_http_next_body_filter = ngx_http_top_body_filter;
+  ngx_http_top_body_filter = ngx_http_jwt_issue_body_filter;
+
+  return NGX_OK;
 }
 
 static ngx_int_t ngx_http_jwt_issue_header_filter(ngx_http_request_t *r) {
@@ -139,7 +164,7 @@ static ngx_int_t ngx_http_jwt_issue_body_filter(ngx_http_request_t *r, ngx_chain
       // TODO(SN): allow to switch JWT algorithm via directive / argument
       // TODO(SN): load key material from directive argument (file)
       char *key = "secretsecretsecretsecretsecret??";
-      if (jwt_set_alg(token, JWT_ALG_HS256, (unsigned char *)key, 32) < 0) {
+      if (jwt_set_alg(token, conf->algorithm, (unsigned char *)key, 32) < 0) {
         // TODO(SN): log error
         return ngx_http_next_body_filter(r, in);
       }
@@ -170,16 +195,4 @@ static ngx_int_t ngx_http_jwt_issue_body_filter(ngx_http_request_t *r, ngx_chain
   }
 
   return ngx_http_next_body_filter(r, in);
-}
-
-// Post configuration - add request handler
-static ngx_int_t ngx_http_jwt_init(ngx_conf_t *cf) {
-  // Install jwt_issue_filter
-  ngx_http_next_header_filter = ngx_http_top_header_filter;
-  ngx_http_top_header_filter = ngx_http_jwt_issue_header_filter;
-
-  ngx_http_next_body_filter = ngx_http_top_body_filter;
-  ngx_http_top_body_filter = ngx_http_jwt_issue_body_filter;
-
-  return NGX_OK;
 }
