04803284745fe1a7e04f135d63d65e07bd2b79ca
Sebastian Nagel <sebastian.nagel@ncoding.at>
2017-05-25 17:30:01 +0200

    Added mock api and written first integration tests

---
+++ test/api/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+let app = express();
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+app.post('/login', (req, res) => {
+  res.json(req.body);
+})
+app.get('/api', (req, res) => {
+  res.json({ message: 'api response' });
+});
+app.post('/api/echo', (req, res) => {
+  res.json(req.body);
+});
+
+app.listen(80);
+console.log('Test api started');
--- test/index.js
+++ test/index.js
@@ -1,17 +1,49 @@
 import assert from 'assert';
 import fetch from 'node-fetch';
 
-describe('jwt_issue', () => {
-  it('should issue a token', () => {
-    return fetch('http://test-api/login', {
-      method : 'POST',
-      body: {
-        user: 'name',
-        password: 'test'
-      }})
-      .then((response) => {
-        assert.equal(200, response.statusCode);
-      })
-      .catch((error) => assert.fail(error));
+describe('nginx_jwt', () => {
+
+  describe('normal proxy', () => {
+    it('works correctly', () => {
+      return fetch('http://nginx-jwt/')
+        .then((response) => {
+          assert.equal(200, response.status);
+        });
+    });
+  });
+
+  describe('jwt_issue', () => {
+
+    function parseBase64(data) {
+      return JSON.parse(new Buffer(data, 'base64').toString('ascii'));
+    }
+
+    function assertToken(expectedBody, data) {
+      const parts = data.split('.');
+      const header = parseBase64(parts[0]);
+      assert.equal("JWT", header.typ);
+      assert.equal("HS512", header.alg);
+      const body = parseBase64(parts[1]);
+      assert.deepEqual(expectedBody, body);
+    }
+
+    it('returns a token in response body', () => {
+      const expected = { user: 'test', password: 'secret' };
+      return fetch('http://nginx-jwt/login', {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(expected)
+      }).then((response) => response.text())
+        .then((data) => assertToken(expected, data));
+    });
+  });
+
+  describe('jwt_verify', () => {
+    it('returns 401 without token', () => {
+      return fetch('http://nginx-jwt/api')
+        .then((response) => {
+          assert.equal(401, response.status);
+        });
+    });
   });
 });
